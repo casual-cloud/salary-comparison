bc25bf00183eab77ff73d288035d2bc352a2398e
Maetad Sukarasud <meatad91@gmail.com>
2025-01-14 00:44:28 +0700

    feat: add gtag

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueGtag from 'vue-gtag'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +11,16 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(
+  VueGtag,
+  {
+    appName: 'Salary Comparison',
+    pageTrackerScreenviewEnabled: true,
+    config: {
+      id: import.meta.env.VITE_MEASUREMENT_ID
+    },
+  },
+  router,
+)
 
 app.mount('#app')
